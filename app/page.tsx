ce8687c213866608c32378aae74f540b8038def9
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-02-05 18:15:32 +0200

    implemented front end

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
+// import Image from "next/image";
+// import AddNote from "@/components/AddNote";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Welcome to Crud with nextjs 14 mongodb prisma axios</h2>
-    </main>
-  );
+// export default function Home() {
+//   return (
+//     <main>
+//       <AddNote />
+//     </main>
+//   );
+// }
+
+import AddNote from '@/components/AddNote'
+import NoteList from '@/components/NoteList'
+
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/post", { cache: 'no-store' });
+
+  if(!res.ok) {
+      throw new Error("Failed to fetch data")
+  }
+
+  return res.json();
 }
+
+
+const Home = async () => {
+  const notes = await getData();
+
+  return (
+    <main>
+      <AddNote />
+      <section>
+        <NoteList notes={notes}  />
+      </section>
+    </main>
+  )
+}
+
+export default Home
